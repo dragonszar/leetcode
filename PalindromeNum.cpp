cc37910e4f84e2a15de68ce6d20a88ebc5d1b8fa
kaixinxiaomao <xdong0214@gmail.com>
2013-08-29 21:03:18 -0400

    Palindrome Number
    Determine whether an integer is a palindrome. Do this without extra
    space.
    
    http://leetcode.com/onlinejudge#question_9

---
+++ PalindromeNum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int ndgt = 0;
+        int cur = x;
+        while(cur>0)
+        {
+            ndgt++;
+            cur = cur/10;
+        }
+
+        
+        for(int i=1; i <= (ndgt+1)/2; i++)
+        {
+            if(((x/(ndgt-i))%10) != ((x%10)/(10^(i-1))))
+                return false; 
+        }
+        return true;
+    }
+    
+    
+};
