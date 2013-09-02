0482e94c2e37a48907de7f9a8c2a5829bc1176df
kaixinxiaomao <xdong0214@gmail.com>
2013-09-02 19:05:56 -0400

    Need to update both the window (pat) and the hash
    Passed all

--- LongestSubstringWithoutRepeat.cpp
+++ LongestSubstringWithoutRepeat.cpp
@@ -8,15 +8,25 @@ public:
         map<char, int> hash;
         int maxlen = 0;
         int len = 0;
+    	string pat = "";
+		int patlen = 0;
         for(int i = 0; i<s.size(); i++)
         {
+			
             if(hash[s[i]])
             {
-                //len = 1;
+				patlen = pat.find_first_of(s[i]);
+				len = len - patlen;	//Within the pattern the chars before last occurance of s[i] will be discarded, both in hash and pat
+				for(int i=0; i<patlen; i++)
+					hash.erase(pat[i]);	//Need to clear the hash
+				pat = pat.substr(patlen+1);	//Need to update the pattern, don't miss adding one
+				pat.append(1, s[i]);
+
             }
             else{
-                len++;
-                hash[s[i]] = 1; //Cannot assigned to i, since i can be 0, and we rely on hash['x']==0 to decide if 'x' hasn't been inserted 
+                hash[s[i]] = 1;//Cannot assigned to i, since i can be 0, and we rely on hash['x']==0 to decide if 'x' hasn't been inserted 
+				pat.append(1, s[i]);
+				len++;
             }
             if(len>maxlen)
                 maxlen = len;
