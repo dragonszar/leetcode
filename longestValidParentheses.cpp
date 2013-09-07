d9d8cbfd55a72e75e0124d91acb4cc2266f38f3d
kaixinxiaomao <xdong0214@gmail.com>
2013-09-07 19:38:13 -0400

    Passed all
    Need to due with missing closing in the end case, which is very hard;
    bypass it by do the same process from right to left (increase credit
    when closing and decrease when opening), which will also have the
    missing opening in the front problem, but we select the bigger solution
    to return.

--- longestValidParentheses.cpp
+++ longestValidParentheses.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
@@ -15,17 +16,68 @@ public:
                     credit--;
                     curlen++;
                 }
-                else{
+                else{   ///extra closing without opening, clear
                     curlen = 0;
+                    credit = 0;
                 }
+                if(credit==0 && curlen > rst)
+                    rst = curlen;
+                //if(credit>0 && curlen - credit > rst)   ///missing closing, passing ((), but failed on ()(()
+                   // rst = curlen - credit;
             }else{
                 credit++;
                 curlen++;
             }
-            if(curlen > rst)
-                rst = curlen;
+            /*
+            if(credit > 0 && i = s.size() -1)   ///missing closing but no more input, fail on ((()(
+            {
+                int j = i - 1;
+                int foundopening = -1
+                while(foundopening < credit)
+                {
+                    if(s[j] == ')')
+                        foundopening--;
+                    else
+                        foundopening++
+                    j--;
+                }
+                      
+            }
+            */
         }
-        rst = rst - credit; //Have unclosed left parenthis
+        
+        if(credit > 0)  ///If there's missing closing but no more input, then do the same process from right to left and select the bigger one to return
+        {
+            int rstfromright = 0;
+            credit = 0;
+            curlen = 0;       
+            for(int i = s.size() - 1; i >=0 ; i--)   ///Start from right, this is to avoid missing solutions when 
+            {
+                if(s[i] == '(')
+                {
+                    if(credit>0)
+                    {
+                        credit--;
+                        curlen++;
+                    }
+                    else{   
+                        curlen = 0;
+                        credit = 0;
+                    }
+                    if(credit==0 && curlen > rstfromright)
+                        rstfromright = curlen;
+
+                }else{
+                    credit++;
+                    curlen++;
+                }
+            }
+            if(rstfromright > rst)
+                    rst = rstfromright;
+        }
+
+
+        //rst = rst - credit; //Have unclosed left parenthis
         return rst; //Don't miss return
     }
 };
