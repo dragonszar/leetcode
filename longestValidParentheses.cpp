a22ecfb2ccb38610187db41aae3d69c7e781db9d
kaixinxiaomao <xdong0214@gmail.com>
2013-09-07 13:18:43 -0400

    Revert "Rollback"
    This reverts commit 700c089a32c54d2841a255c0a3fa544872a6a194.

--- longestValidParentheses.cpp
+++ longestValidParentheses.cpp
@@ -1,53 +1,31 @@
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if(s == "")
-            return 0;
+        int rst = 0;
         int credit = 0;
-        vector<int> buf(s.size(), 0);
+        int curlen = 0;
         for(int i = 0; i < s.size(); i++)
         {
-            if(s[i] == '(')
+            if(s[i] == ')')
             {
-                credit++;
+                if(credit>0)
+                {
+                    credit--;
+                    curlen++;
+                }
+                else{
+                    curlen = 0;
+                }
             }else{
-                credit--;
-                //if(credit < 0)
-                    //credit = 0;
+                credit++;
+                curlen++;
             }
-            buf[i] = credit;
+            if(curlen > rst)
+                rst = curlen;
         }
-        vector<int> dif(s.size(), 0);
-        for(int i = 0; i < s.size()-1; i++)
-        {
-            dif[i] = buf[i+1] - buf[i];
-        }
-        //dif[s.size()-1] = 0;
-        vector<int> diff(s.size(), 0);
-        for(int i = 1; i < s.size(); i++)
-        {
-            diff[i] = dif[i-1] - dif[i];
-        }
-        //diff[0] = 0;
-        int peeki = 0, btmi = 0;
-        int maxdiff =diff[0], mindiff = diff[0]; 
-        for(int i = 1; i < s.size(); i++)
-        {
-            if(diff[i] > maxdiff)
-            {
-                maxdiff = diff[i];
-                peeki = i;
-            }
-            if(diff[i] < mindiff)
-            {
-                mindiff = diff[i];
-                btmi = i;
-            }           
-        }
-
-        return (buf[peeki]-buf[btmi]) * 2; //Don't miss return
+        rst = rst - credit; //Have unclosed left parenthis
+        return rst; //Don't miss return
     }
 };
