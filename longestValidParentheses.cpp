700c089a32c54d2841a255c0a3fa544872a6a194
kaixinxiaomao <xdong0214@gmail.com>
2013-09-07 13:18:38 -0400

    Rollback

--- longestValidParentheses.cpp
+++ longestValidParentheses.cpp
@@ -1,31 +1,53 @@
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int rst = 0;
+        if(s == "")
+            return 0;
         int credit = 0;
-        int curlen = 0;
+        vector<int> buf(s.size(), 0);
         for(int i = 0; i < s.size(); i++)
         {
-            if(s[i] == ')')
+            if(s[i] == '(')
             {
-                if(credit>0)
-                {
-                    credit--;
-                    curlen++;
-                }
-                else{
-                    curlen = 0;
-                }
-            }else{
                 credit++;
-                curlen++;
+            }else{
+                credit--;
+                //if(credit < 0)
+                    //credit = 0;
             }
-            if(curlen > rst)
-                rst = curlen;
+            buf[i] = credit;
         }
-        rst = rst - credit; //Have unclosed left parenthis
-        return rst; //Don't miss return
+        vector<int> dif(s.size(), 0);
+        for(int i = 0; i < s.size()-1; i++)
+        {
+            dif[i] = buf[i+1] - buf[i];
+        }
+        //dif[s.size()-1] = 0;
+        vector<int> diff(s.size(), 0);
+        for(int i = 1; i < s.size(); i++)
+        {
+            diff[i] = dif[i-1] - dif[i];
+        }
+        //diff[0] = 0;
+        int peeki = 0, btmi = 0;
+        int maxdiff =diff[0], mindiff = diff[0]; 
+        for(int i = 1; i < s.size(); i++)
+        {
+            if(diff[i] > maxdiff)
+            {
+                maxdiff = diff[i];
+                peeki = i;
+            }
+            if(diff[i] < mindiff)
+            {
+                mindiff = diff[i];
+                btmi = i;
+            }           
+        }
+
+        return (buf[peeki]-buf[btmi]) * 2; //Don't miss return
     }
 };
