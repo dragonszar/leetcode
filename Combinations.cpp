1a65634ca4c2f0c4a3f1a3ac354d94e6829dd936
kaixinxiaomao <xdong0214@gmail.com>
2013-10-07 20:54:46 -0400

    Code review

--- Combinations.cpp
+++ Combinations.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     vector<vector<int> > combine(int n, int k) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+
         //if(k < n/2)
             //k = n-k;
         rslt.clear();	//so that one Solution object can make multiple call on multiple inputs, like online judge
         vector<bool> bitmap(n + 1, false);
-        slns = &bitmap;
+        slns = &bitmap;	//Since our length of the bitmap is decided by n per each call, we define the bitmap as a pointer which will point to a per-call based local var
         comb(n, k, 0, 1);
         return rslt;
     }
@@ -15,9 +14,9 @@ public:
     vector<bool> *slns;
     vector<vector<int>> rslt;
     
-    void comb(int n, int k, int ki, int stt)
+    void comb(int n, int k, int ki, int stt)	///n is n; k is k; ki is the current # of choice, ki <= k; all integer in [stt, n] will be options for next choice  
     {
-        if(ki == k)
+        if(ki == k)	///When made all k choices, convert the bit map to a solution and add to solution set
         {
             vector<int> rst;
             for(int i = 1; i <= n; i++)
@@ -32,10 +31,10 @@ public:
 
         for(int j = stt; j <= n; j++ )
         {
-            if((*slns)[j])
+            /*if((*slns)[j])
 			{
 				continue;
-			}
+			}*/
             (*slns)[j] = true;
             comb(n, k, ki + 1, ++stt);	//++stt is the key point!!
             (*slns)[j] = false;		//Only reset the bit when visited
--- Subsets.cpp
+++ Subsets.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
+
         sort(S.begin(), S.end(), compar());
         
         return subsets_rec(S);
     }
     
     vector<vector<int> > subsets_rec(vector<int> &S) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
 
         if(S.size()==0) //Need to create a local to return
         {   
@@ -23,8 +19,8 @@ public:
         
           
         int frst =  S[0];
-        S.erase(S.begin());
-        vector<vector<int>> rtns = subsets(S);
+        S.erase(S.begin());	//Or can use increasing index like careercup does
+        vector<vector<int>> rtns = subsets_rec(S);
         vector<vector<int>> rtnscpy =  rtns;	//Cannot directly loop on rtns, since pushing to rtns will invalidate the iterator
         for(vector<vector<int>>::iterator it = rtnscpy.begin(); it!=rtnscpy.end(); ++it)
         {
